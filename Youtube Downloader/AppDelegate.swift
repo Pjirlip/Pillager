8fb783fab0c9f3383a2565f8b6eb6502673e4620
PhilippD <dippelp@hochschule-trier.de>
2017-01-10 17:58:57 +0100

    Initial Commit

---
+++ Youtube Downloader/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Youtube Downloader
+//
+//  Created by Philipp Dippel on 10.01.17.
+//  Copyright © 2017 Philipp Dippel. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Youtube Downloader/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Youtube Downloader
+//
+//  Created by Philipp Dippel on 10.01.17.
+//  Copyright © 2017 Philipp Dippel. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
