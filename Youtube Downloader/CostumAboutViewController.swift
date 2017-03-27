1e4a6bd3060000a89fe027908ad21ae1c88ba4b6
PhilippD <dippelp@hochschule-trier.de>
2017-03-27 22:01:45 +0200

    costum about

---
+++ Youtube Downloader/CostumAboutViewController.swift
@@ -0,0 +1,24 @@
+//
+//  CostumAboutViewController.swift
+//  Pillager
+//
+//  Created by Philipp Dippel on 27.03.17.
+//  Copyright © 2017 Philipp Dippel. All rights reserved.
+//
+
+import Cocoa
+
+class CostumAboutViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+        
+    
+        
+        
+    }
+    
+ 
+    
+}
